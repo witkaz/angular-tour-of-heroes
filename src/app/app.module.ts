798eec86fd76a7edd5c94beb578f05b9ec60d728
Witold Podskarbi <witold.podskarbi@volvo.com>
2018-10-09 11:31:09 +0200

    create message component, message servie, modify getHeroes method with observable and subscribe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './features/heroes/heroes.component';
+import { HeroDetailComponent } from './features/hero-detail/hero-detail.component';
+import { MessagesComponent } from './features/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/features/hero-detail/hero-detail.component.ts
+++ src/app/features/hero-detail/hero-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { Hero } from '../hero';
+import { Hero } from '../../hero';
 
 @Component({
   selector: 'app-hero-detail',
